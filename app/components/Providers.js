9cc4332ea5dc0b5574540b9f91b03819e3cc4053
bulbashenko <bulbashenko69@gmail.com>
2024-11-04 01:58:05 +0100

    Added provider || Maybe remove later

---
+++ app/components/Providers.js
@@ -0,0 +1,13 @@
+// components/Providers.js
+
+"use client"
+
+import { SessionProvider } from "next-auth/react"
+
+export default function Providers({ children }) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
